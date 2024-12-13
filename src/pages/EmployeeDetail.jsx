fc367fc5419b7898a529a8dfdb29691b6bdcdf11
Amannigam1820 <amannigam18082001@gmail.com>
2024-12-13 11:59:46 +0530

    UI with take input as doc in editor and generate pdf file

--- src/pages/EmployeeDetail.jsx
+++ src/pages/EmployeeDetail.jsx
@@ -225,7 +225,7 @@ const EmployeeDetailPage = () => {
                   to={`/resume/${employee.id}`} // Updated to redirect to the /resume page
                   className="bg-blue-500 text-white px-4 py-2 rounded"
                 >
-                  edit Resume
+                  Edit Resume
                 </Link>
                 {/* <Link
                   // href={employee.resume}
